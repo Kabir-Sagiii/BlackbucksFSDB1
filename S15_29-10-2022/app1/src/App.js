de3dfbcdbb494e3efa5fb773b37e8bf70d83eea1
Kabir <sagithedeveloper@gmail.com>
2022-10-29 10:01:20 +0530

    added the 29-10-2022 code and paint file

---
+++ S15_29-10-2022/app1/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Headers/Header";
+import Main from "./Components/Main/Main.js";
+
+function App() {
+  // className = class
+  return (
+    <div className="App" id="">
+      <Header />
+
+      <Main />
+    </div>
+  );
+}
+
+export default App;
